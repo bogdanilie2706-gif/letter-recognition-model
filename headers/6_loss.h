7d5151e2e87af70499ccd26897eea9e3cb40fa82
Ilie Bogdan <bogdan.ilie2706@gmail.com>
2026-07-14 18:49:19 +0300

    completed the entropy loss and gradient functions + added math.h lib

--- headers/6_loss.h
+++ headers/6_loss.h
@@ -2,6 +2,7 @@
 #define LOSS_H
 
 #include "../headers/2_matrix.h"
+#include <math.h>
 
 // for monitoring progress, not for actual training
 float cross_entropy_loss(matrix_t predicted, matrix_t target);
--- source/6_loss.c
+++ source/6_loss.c
@@ -2,10 +2,28 @@
 
 float cross_entropy_loss(matrix_t predicted, matrix_t target)
 {
+	if (predicted->cols != target->cols || predicted->rows != target->rows) {
+		perror("predicted and target have different dimensions in cross_entropy_loss");
+		return NAN;
+	}
 
+	float loss = 0.0f;
+	for (int i = 0; i < predicted->rows; i++) {
+		for (int j = 0; j < predicted->cols; j++) {
+			loss -= target->data[i * predicted->cols + j] * 
+				logf(predicted->data[i * predicted->cols + j] + 1e-9f);
+			// added 1e-9 to avoid log(0)
+		}
+	}
+	loss /= predicted->rows;
+	return loss;
 }
 
 matrix_t cross_entropy_gradient(matrix_t predicted, matrix_t target)
 {
-    
+	if (predicted->cols != target->cols || predicted->rows != target->rows) {
+		perror("predicted and target have different dimensions in cross_entropy_gradient");
+		return NULL;
+	}
+	return subtract_matrix(predicted, target);
 }
