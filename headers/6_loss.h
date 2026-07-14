b439f08b08a5bed1cb52bf987e2e24dca4301907
Ilie Bogdan <bogdan.ilie2706@gmail.com>
2026-07-14 18:20:11 +0300

    written the loss functions headers + comments

---
+++ headers/6_loss.h
@@ -0,0 +1,12 @@
+#ifndef LOSS_H
+#define LOSS_H
+
+#include "../headers/2_matrix.h"
+
+// for monitoring progress, not for actual training
+float cross_entropy_loss(matrix_t predicted, matrix_t target);
+
+// computes predictet - target for backpropagation
+matrix_t cross_entropy_gradient(matrix_t predicted, matrix_t target);
+
+#endif
---
+++ source/6_loss.c
@@ -0,0 +1,11 @@
+#include "../headers/6_loss.h"
+
+float cross_entropy_loss(matrix_t predicted, matrix_t target)
+{
+
+}
+
+matrix_t cross_entropy_gradient(matrix_t predicted, matrix_t target)
+{
+    
+}
